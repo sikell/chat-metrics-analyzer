0c082fff07c2f270b77941929f4ba94d8fdc403e
Simon Keller <simonkeller94@aol.com>
2018-03-29 15:32:33 +0200

    Add simple hello world rest endpoint

---
+++ src/main/kotlin/de/sikeller/tools/chatMetricsAnalzyer/api/AnalyzeController.kt
@@ -0,0 +1,15 @@
+package de.sikeller.tools.chatMetricsAnalzyer.api
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("analyze")
+class AnalyzeController {
+
+    @GetMapping("/")
+    fun analyze(): String {
+        return "Hello world"
+    }
+}
